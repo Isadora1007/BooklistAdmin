c5ca063ad904a6f1ffb6e188e8648b15d94d2e00
Alfonzo <alfonzo.angulodelacruz@calgarylibrary.ca>
2019-12-02 14:46:26 -0700

    Clear users' data from state and JWt from local storage

--- BooklistAdmin/ClientApp/src/components/SignOutBtn.js
+++ BooklistAdmin/ClientApp/src/components/SignOutBtn.js
@@ -1,15 +1,29 @@
-﻿import React from 'react'
+﻿import React, { useContext } from 'react'
 import Button from '@material-ui/core/Button'
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
+import { Ctx } from '../Context'
 
 const SignOutBtn = () => {
-
+    const celarUserData = {
+        authenticateToken: null,
+        authenticated: false,
+        displayName: "",
+        message: null,
+        password: null,
+        userName: "", 
+    }
+    const { setGobalValue } = useContext(Ctx)
+    const signOut = () => {
+        localStorage.clear()
+        setGobalValue('userData', celarUserData)
+    }
 
     return (
         <Button
             variant="contained"
             color="primary"
             disabled={false}
+            onClick={() => signOut()}
             component={Link} to='sign-in'>
             Sign-out
         </Button>
