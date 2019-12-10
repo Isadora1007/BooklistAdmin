9460c1ea3471be14656759dbc89a3376f6dab996
Alfonzo <alfonzo.angulodelacruz@calgarylibrary.ca>
2019-12-10 14:40:44 -0700

    Added drop-down menu, with all of the branches, to the BookListForm page.

--- BooklistAdmin/ClientApp/src/pages/BookListForm.js
+++ BooklistAdmin/ClientApp/src/pages/BookListForm.js
@@ -5,10 +5,17 @@ import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
+import Branches from '../components/Select'
 import Button from '@material-ui/core/Button'
 import SetNewBookList from '../api/SetNewBookList'
 import getBookList from '../api/getBookList'
 import editBookList from '../api/editBookList'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
 import { Ctx } from '../Context'
 
 const useStyles = makeStyles(theme => ({
@@ -23,29 +30,45 @@ const BookListForm = (props) => {
     const { state, setGobalValue } = useContext(Ctx)
     const classes = useStyles()
     let history = useHistory()
-    
+
+    //Drop-down states
+    const [branchList, setBranchList] = React.useState({ ListOfItems: [{ Name: 'None', Code: 'none' }] })
+    const inputLabel = React.useRef(null)
+    const [labelWidth, setLabelWidth] = React.useState(0)
+
     const [formState, setFormState] = useState({
         formInputs: {
             title: '',
             owner: '',
             description: '',
-            bibliocommonslisturl: ''
+            bibliocommonslisturl: '',
+            branch: '',
+            active: false
         }
-   })
+    })
 
     useEffect(() => {
+        setLabelWidth(inputLabel.current.offsetWidth)
         const fetchBookLists = async () => {
             const resposne = await getBookList(bookListId)
             setFormState({ ...formState, formInputs: resposne })
         }
         if (state.editMode) {
-            fetchBookLists()   
+            fetchBookLists()
         }
-        
+        const fetchBranchList = async () => {
+            const requestData = await fetch('https://reg.calgarylibrary.ca/web-services?sn=Branches')
+            const response = await requestData.text()
+            setBranchList(JSON.parse(response))
+        }
+        fetchBranchList()
     }, [])
 
     const handleChange = (e, name) => {
-      setFormState({ formInputs: { ...formState.formInputs, [name]: e.target.value } })
+        setFormState({ formInputs: { ...formState.formInputs, [name]: e.target.value } })
+    }
+    const handleSwitchChange = (e, name) => {
+        setFormState({ formInputs: { ...formState.formInputs, [name]: e.target.checked } })
     }
 
     const submitForm = () => {
@@ -110,9 +133,33 @@ const BookListForm = (props) => {
                             variant="outlined"
                             onChange={e => handleChange(e, 'owner')}
                         />
+                        <FormControl variant="outlined" fullWidth>
+                            <InputLabel ref={inputLabel} id="demo-simple-select-outlined-label">
+                                Select a branch
+                            </InputLabel>
+                            <Select
+                                labelId="demo-simple-select-outlined-label"
+                                id="demo-simple-select-outlined"
+                                value={formState.formInputs.branch}
+                                onChange={e => handleChange(e, 'branch')}
+                                labelWidth={labelWidth}
+                            >
+                                {
+                                    branchList.ListOfItems.map(b => <MenuItem key={b.Code} value={b.Code}>{b.Name}</MenuItem>)
+                                }
+
+                            </Select>
+                        </FormControl>
+
+                        <FormControlLabel
+                            control={
+                                <Switch checked={formState.formInputs.active} onChange={e => handleSwitchChange(e, 'active')} value="active" />
+                            }
+                            label="Active"
+                        />
                     </form>
-                    <Button variant="contained" color="primary" disabled={false} onClick={() => { submitForm()}}>
-                        { cta }
+                    <Button variant="contained" color="primary" disabled={false} onClick={() => { submitForm() }}>
+                        {cta}
                     </Button>
                     <Button variant="contained" onClick={() => { handleCancel() }}>
                         Cancel
